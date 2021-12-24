0d653e95217b5b273ec0b2a3a3019e101d21c282
JoseEPina <jose.edpina@gmail.com>
2021-12-23 20:47:22 -0700

    Initial File Setup, based on Deep Thoughts module.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+   uri: '/graphql',
+});
+
+//* With the configuration of authLink, we use the setContext() function to retrieve the token from localStorage and
+//* set the HTTP request headers of every request to include the token, whether the request needs it or not. This is
+//* fine, because if the request doesn't need the token, our server-side resolver function won't check for it.
+//*
+//* We wont use the first parameter of setContext(), which stores the current request object in case this function
+//* is running after we've initiated a request. Because we're not using the first parameter, but we still need to
+//* access the second one, we can use an underscore _ to serve as a placeholder for the first parameter.
+const authLink = setContext((_, { headers }) => {
+   const token = localStorage.getItem('id_token');
+   return {
+      headers: {
+         ...headers,
+         authorization: token ? `Bearer ${token}` : '',
+      },
+   };
+});
+
+//* use ApolloClient() constructor to instantiate the Apollo Client and create the connection to the API endpoint
+//*
+//* link: authLink.concat(httpLink) - combine the authLink and httpLink objects so that every request retrieves the
+//* token and sets the request headers before making the request to the API. With this in place, we
+//* won't have to worry about doing this manually with every single request. It'll just do it for us.
+const client = new ApolloClient({
+   link: authLink.concat(httpLink),
+   cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <ApolloProvider client={client}>
+         <Router>
+            <div className='flex-column justify-flex-start min-100-vh'>
+               <Header />
+               <div className='container'>
+                  <Switch>
+                     <Route exact path='/' component={Home} />
+                     <Route exact path='/login' component={Login} />
+                     <Route exact path='/signup' component={Signup} />
+                     <Route exact path='/profile/:username?' component={Profile} />
+                     <Route exact path='/thought/:id' component={SingleThought} />
+
+                     <Route component={NoMatch} />
+                  </Switch>
+               </div>
+               <Footer />
+            </div>
+         </Router>
+      </ApolloProvider>
+   );
 }
 
 export default App;
---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+// import path module for express
+const path = require('path');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+// import supposed middleware function...
+const { authMiddleware } = require('./utils/auth');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+   // create a new Apollo server and pass in our schema data
+   const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware,
+   });
+
+   //* Start the Apollo server
+   await server.start();
+
+   // Integrate our Apollo server with the Express application as middleware
+   server.applyMiddleware({ app });
+
+   // log where we can go to test oue GWL API
+   console.log(`Use GrapgQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+//* Initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//* Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+   app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+   });
+});
