39f2d5c9c8f32fdc0359cb07b512d2d1ce295e2c
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-12-26 15:40:13 -0700

    added models

---
+++ server/models/Provider.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
+const { Provider } = require(".");
+
+const providerSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  toJSON: {
+    virtuals: true,
+  },
+});
+
+// set up pre-save middleware to create password
+providerSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+providerSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+providerSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const Provider = model("User", providerSchema);
+
+module.exports = Provider;
